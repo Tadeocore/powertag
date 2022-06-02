38c6c883ccaeda995d698fc59372fb78d872f1cc
White-Byakko <waly.leo.31@gmail.com>
2022-06-02 19:29:56 -0300

    prototipoSalto

--- Assets/Scripts/Movimiento Rigidbody/MovementCharacter.cs
+++ Assets/Scripts/Movimiento Rigidbody/MovementCharacter.cs
@@ -13,6 +13,9 @@ public class MovementCharacter : MonoBehaviour
 	
 	public float rotacion = 90;
 
+	public float fSalto = 100;
+	public float tSalto = 0;
+
 	void Start()
 	{
 
@@ -23,16 +26,23 @@ public class MovementCharacter : MonoBehaviour
 
 	void FixedUpdate()
 	{
-
 		//Capturo el movimiento en horizontal y vertical de nuestro teclado
 		float movimientoH = Input.GetAxis("Horizontal");
 		float movimientoV = Input.GetAxis("Vertical");
 		float movimientoA = 1.5f;
-
-		//Genero el vector de movimiento asociado, teniendo en cuenta la velocidad
+		
+		
+		
 		Vector3 movimiento = new Vector3(movimientoH * velocidad, movimientoA, movimientoV * velocidad);
 		transform.rotation = Quaternion.Euler(0f, movimientoH*rotacion, 0f);
-		
+
+		tSalto = tSalto + Time.deltaTime;
+		if (Input.GetKeyDown(KeyCode.Space) && tSalto > 1)
+		{
+			transform.Translate(0, fSalto * Time.deltaTime, 0);
+			tSalto = 0;
+		}
+
 		if(movimientoV == -1) transform.rotation = Quaternion.Euler(0f, 180f, 0f);
 
 		//Aplico ese movimiento al RigidBody del jugador
